aa4025ab5f9d081464d049d749da1408c877badf
Rho <rhoparkour@gmail.com>
2013-11-22 14:08:16 -0800

    Added missing file

---
+++ src/medium/medium_vars.f90
@@ -0,0 +1,11 @@
+  module md_vars
+  implicit none
+
+! Properties of the ISM, hot bubble and driving energy
+  real    :: md_gamma_ism
+  real    :: md_gamma_bub
+  real    :: md_kappa
+  real    :: md_factor
+  real    :: md_ism_dens
+  real    :: md_xi,md_eta,md_zeta,md_A,md_B
+  end module md_vars
